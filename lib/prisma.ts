74f40e9252bf9236a9a7014bd652369c54e7722f
Dydy45 <dylndjoko@gmail.com>
2026-01-29 15:33:44 +0100

    Essaie de la nouvelle version de prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,23 @@
-import { PrismaClient } from "../app/generated/prisma/client";
+import 'dotenv/config'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import { PrismaClient } from '../app/generated/prisma/client'
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
+  const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL || 'file:./dev.db',
+  })
+
+  return new PrismaClient({ adapter })
 }
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+const globalForPrisma = globalThis as unknown as {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined
+}
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalForPrisma.prismaGlobal ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prismaGlobal = prisma
+}
 
 export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
