ed61bdb9a5eee90a10fdcefc09fabf9569bd491f
Dydy45 <dylndjoko@gmail.com>
2026-01-29 16:24:01 +0100

    Mise a jour de prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,10 @@
-import 'dotenv/config'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 import { PrismaClient } from '../app/generated/prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
 const prismaClientSingleton = () => {
   const adapter = new PrismaBetterSqlite3({
     url: process.env.DATABASE_URL || 'file:./dev.db',
   })
-
   return new PrismaClient({ adapter })
 }
 
