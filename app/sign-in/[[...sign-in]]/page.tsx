68a06a9fcfec250ce594a8ed232efa6f7d4685e3
Dydy45 <dylndjoko@gmail.com>
2026-01-29 12:18:00 +0100

    custom des pages sign-in et sign-up

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,9 +3,13 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <SignIn />
-    </div>
+    <div className='hero bg-base-200 min-h-screen'>
+          <div className='hero-content text-center'>
+            <div className='max-w-md'>
+                <SignIn />
+            </div>
+          </div>
+        </div>
   )
 }
 
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,8 +3,12 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-      <SignUp />
+    <div className='hero bg-base-200 min-h-screen'>
+      <div className='hero-content text-center'>
+        <div className='max-w-md'>
+            <SignUp />
+        </div>
+      </div>
     </div>
   )
 }
