813d50e3f1bedefba92e28def1fc146938299ef5
Dydy45 <dylndjoko@gmail.com>
2026-01-30 10:25:41 +0100

    Petit essai en rammenant l'ancian fichier prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../app/generated/prisma/client'
+/*import { PrismaClient } from '../app/generated/prisma/client'
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
 const prismaClientSingleton = () => {
@@ -18,4 +18,24 @@ if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prismaGlobal = prisma
 }
 
+export default prisma*/
+
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaClient } from '../app/generated/prisma/client'
+
+const prismaClientSingleton = () => {
+  const adapter = new PrismaBetterSqlite3({
+    url: process.env.DATABASE_URL || 'file:./dev.db',
+  })
+  return new PrismaClient({ adapter })
+}
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
 export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
