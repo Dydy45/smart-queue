07f27c889cb21e4431e17c23603551e5df9a5aac
Dydy45 <dylndjoko@gmail.com>
2026-01-30 12:16:16 +0100

    Creation de la page service

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -5,9 +5,23 @@ const page = () => {
   return (
     <Wrapper>
       <div className='flex w-full'>
-        <div>
+
+        <div className='space-y-2 w-1/4'>
+
             <span className='label-text'>Nom Du Service</span>
+            <div>
+                <input type="text" name="" placeholder='Nom du service' className='input input-bordered input-sm w-full' />
+            </div>
+
+            <span className='label-text'>Temps Moyen (en minutes)</span>
+            <label className='input input-bordered flex items-center input-sm gap-2'>
+                Temps Moyen
+                <input type="number" name="" placeholder='Temps moyen' className='input-sm w-full' />
+            </label>
+            <button className='btn btn-primary btn-sm mt-4 w-full'>Ajouter le service</button>
         </div>
+
+
       </div>
     </Wrapper>
   )
