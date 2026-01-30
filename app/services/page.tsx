7ebb54d197ad38962169b3ca0d33aaf674f84d47
Dydy45 <dylndjoko@gmail.com>
2026-01-30 12:36:47 +0100

    Envoi du service dans la base de données

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client"
+import React, { useState } from 'react'
 import Wrapper from '../components/Wrapper'
+import { useUser } from '@clerk/nextjs'
+import { createService } from '../actions'
 
 const page = () => {
+
+    const {user} = useUser()
+    const email = user?.primaryEmailAddress?.emailAddress
+
+    const [serviceName, setServiceName] = useState("")
+    const [avgTime, setAvgTime] = useState(0)
+
+    const handleCreateService = async () => {
+        if (email && serviceName && avgTime > 0) {
+          try {
+            await createService(email, serviceName, avgTime)
+            setAvgTime(0)
+            setServiceName("")
+          } catch (error) {
+            console.error("Error creating service:", error)
+          }
+        }
+    }
+
+
   return (
     <Wrapper>
       <div className='flex w-full'>
@@ -10,15 +34,15 @@ const page = () => {
 
             <span className='label-text'>Nom Du Service</span>
             <div>
-                <input type="text" name="" placeholder='Nom du service' className='input input-bordered input-sm w-full' />
+                <input type="text" name="" placeholder='Nom du service' className='input input-bordered input-sm w-full' value={serviceName} onChange={(e) => setServiceName(e.target.value)} />
             </div>
 
             <span className='label-text'>Temps Moyen (en minutes)</span>
             <label className='input input-bordered flex items-center input-sm gap-2'>
                 Temps Moyen
-                <input type="number" name="" placeholder='Temps moyen' className='input-sm w-full' />
+                <input type="number" name="" placeholder='Temps moyen' className='input-sm w-full' value={avgTime || ''} onChange={(e) => setAvgTime(Number(e.target.value))} />
             </label>
-            <button className='btn btn-primary btn-sm mt-4 w-full'>Ajouter le service</button>
+            <button className='btn btn-primary btn-sm mt-4 w-full' onClick={handleCreateService}>Ajouter le service</button>
         </div>
 
 
